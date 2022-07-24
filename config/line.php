48292ad355bc703678a00caaac3812e5c4260e21
vanchaiy <65208588+vanchaiy@users.noreply.github.com>
2022-07-24 15:00:26 +0700

    Update line.php

--- config/line.php
+++ config/line.php
@@ -3,8 +3,8 @@
 return [
 
     'channelID' => env('LINE_BOT_CHANNEL_ID', null),
-    'channelSecret' => env('LINE_BOT_CHANNEL_TOKEN', null),
-    'channelAccessToken' => env('LINE_BOT_CHANNEL_SECRET', null),
+    'channelSecret' => env('LINE_BOT_CHANNEL_SECRET', null),
+    'channelAccessToken' => env('LINE_BOT_CHANNEL_TOKEN', null),
 
     'accessToken' => env('LINE_NOTIFY_TOKEN', null),
 
